5620ee92e81805ee060de9fad9bce766563ffd9b
Phillip Conrad <pconrad@cs.ucsb.edu>
2022-04-14 12:30:21 -0700

    pc - issues in order, add to kanban board in reverse

--- kanban.mjs
+++ kanban.mjs
@@ -4,41 +4,44 @@ import glob from 'glob'
 import fs from 'fs/promises';
 
 if (process.argv.length < 4) {
-    console.log("Usage: node kanban.mjs path kanban-board")
-    process.exit(1);
+  console.log("Usage: node kanban.mjs path kanban-board")
+  process.exit(1);
 }
 
 const srcDir = process.argv[2]
 const kanbanBoard = process.argv[3]
 
 const main = async () => {
-    console.log(createPrefix(kanbanBoard));
-    glob(`${srcDir}/*.md`, async (err, files) => {
-        if (err) {
-            console.log(err)
-        } else {
-            var step = 1;
-            var filesReversed = [];
-            for (const filename of files) {
-              filesReversed.unshift(filename);
-            }
-            for (const filename of filesReversed) {
-                const text = await createIssueStep(step, filename, kanbanBoard);
-                console.log(text);
-                step++;
-            }
-        }
-    })
+  console.log(createPrefix(kanbanBoard));
+  glob(`${srcDir}/*.md`, async (err, files) => {
+    if (err) {
+      console.log(err)
+    } else {
+      var step = 1;
+      var filesReversed = [];
+      for (const filename of files) {
+        const text = await createIssueStep(step, filename, kanbanBoard);
+        console.log(text);
+        filesReversed.unshift(filename);
+        step++;
+      }
+      for (const filename of filesReversed) {
+        const text = await addIssueToKanbanBoardStep(step, filename, kanbanBoard);
+        console.log(text);
+        step++;
+      }
+    }
+  })
 }
 
 
 async function getFirstLine(filePath) {
-    const fileContent = await fs.readFile(filePath, 'utf-8');
-    return (fileContent.match(/(^.*)/) || [])[1] || '';
-} 
+  const fileContent = await fs.readFile(filePath, 'utf-8');
+  return (fileContent.match(/(^.*)/) || [])[1] || '';
+}
 
 const createPrefix = (kanbanBoard) => {
-    return `
+  return `
 name: Create Issue From File Example Command
 on:
   workflow_dispatch:
@@ -66,25 +69,26 @@ jobs:
 
 
 const createIssueStep = async (step, filename, project) => {
-    const firstLine = await getFirstLine(filename);
-    return `
+  const firstLine = await getFirstLine(filename);
+  return `
     - name: Create Issue ${step} From File
       id: step${step}
       uses: peter-evans/create-issue-from-file@v3
       with:
         title: ${firstLine}
-        content-filepath: ${filename}
-    - name: Add Issue ${step} to Kanban board ${project}
-      uses: peter-evans/create-or-update-project-card@v1
-      with:
-        project-name: ${project}
-        column-name: Todo
-        issue-number: \${{ steps.step${step}.outputs.issue-number }}`;
-
-
+        content-filepath: ${filename}`;
 }
 
-
+const addIssueToKanbanBoardStep = async (step, filename, project) => {
+  const firstLine = await getFirstLine(filename);
+  return `
+  - name: Add Issue ${step} to Kanban board ${project}
+    uses: peter-evans/create-or-update-project-card@v1
+    with:
+      project-name: ${project}
+      column-name: Todo
+      issue-number: \${{ steps.step${step}.outputs.issue-number }}`;
+}
 
 
 await main();
